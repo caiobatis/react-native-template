25c28fd6712146243a5050bf1793674ab068f351
Caio Batista <caiobatis7@gmail.com>
2022-01-21 18:16:33 -0300

    :art: add axios client and dotenv setup

---
+++ react-native-dotenv.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-dotenv' {
+  export const API_BASE_URL: string
+  export const SITE_BASE_URL: string
+  export const HUB_SPOT_PORTAL_ID: string
+  export const HUB_SPOT_FORM_ID: string
+}
--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,1 +1,12 @@
-export default {}
+import axios from 'axios'
+import { API_BASE_URL } from 'react-native-dotenv'
+
+const client = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 8000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default client
